7fbfbb2172b31e5fe5e5b888eede736f42201139
bhrum <bhrumom@gmail.com>
2026-06-11 09:21:02 +0800

    Add T0257 merit benefits data (#843)
    * Add T0257 merit benefits data
    
    * Register T0257 merit benefits data

--- frontend/apps/web/src/lib/faliu-merit-benefits.ts
+++ frontend/apps/web/src/lib/faliu-merit-benefits.ts
@@ -33,6 +33,7 @@ import { T0253_JUAN_1_MERIT_BENEFITS } from "./faliu-merit-benefits-t0253";
 import { T0254_JUAN_1_MERIT_BENEFITS } from "./faliu-merit-benefits-t0254";
 import { T0255_JUAN_1_MERIT_BENEFITS } from "./faliu-merit-benefits-t0255";
 import { T0256_JUAN_1_MERIT_BENEFITS } from "./faliu-merit-benefits-t0256";
+import { T0257_JUAN_1_MERIT_BENEFITS } from "./faliu-merit-benefits-t0257";
 import { T0262_JUAN_7_MERIT_BENEFITS } from "./faliu-merit-benefits-t0262";
 import { T0366_JUAN_1_MERIT_BENEFITS } from "./faliu-merit-benefits-t0366";
 import { T1153_JUAN_1_MERIT_BENEFITS, T1153_JUAN_2_MERIT_BENEFITS } from "./faliu-merit-benefits-t1153";
@@ -319,6 +320,10 @@ export function getFaliuMeritBenefits(work: string | null | undefined, juan: str
 
   if (normalizedWork === "T0256" && normalizedJuan === "1") {
     return T0256_JUAN_1_MERIT_BENEFITS;
+  }
+
+  if (normalizedWork === "T0257" && normalizedJuan === "1") {
+    return T0257_JUAN_1_MERIT_BENEFITS;
   }
 
   if (normalizedWork === "T0262" && normalizedJuan === "7") {
