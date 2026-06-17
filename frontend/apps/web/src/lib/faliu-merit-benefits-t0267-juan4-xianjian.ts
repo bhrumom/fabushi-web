a9045721e0b4082124cec9d85471597d2fd878ff
bhrum <bhrumom@gmail.com>
2026-06-18 06:22:31 +0800

    Add T0267 juan 4 conclusion merit benefits (#1042)
    * Add T0267 juan 4 conclusion merit benefits
    
    * Include T0267 juan 4 conclusion merit benefits

--- frontend/apps/web/src/lib/faliu-merit-benefits-t0267-juan4-xianjian.ts
+++ frontend/apps/web/src/lib/faliu-merit-benefits-t0267-juan4-xianjian.ts
@@ -1,6 +1,7 @@
 import type { FaliuMeritBenefit } from "./faliu-merit-benefits";
 import { T0267_JUAN_4_ANYANG_FINAL_MERIT_BENEFITS } from "./faliu-merit-benefits-t0267-juan4-anyang-final";
 import { T0267_JUAN_4_ANYANG_LASTAGE_MERIT_BENEFITS } from "./faliu-merit-benefits-t0267-juan4-anyang-lastage";
+import { T0267_JUAN_4_ANYANG_CONCLUSION_MERIT_BENEFITS } from "./faliu-merit-benefits-t0267-juan4-anyang-conclusion";
 import { T0267_JUAN_4_ANYANG_WOMEN_MERIT_BENEFITS } from "./faliu-merit-benefits-t0267-juan4-anyang-women";
 
 export const T0267_JUAN_4_XIANJIAN_MERIT_BENEFITS: FaliuMeritBenefit[] = [
@@ -127,4 +128,5 @@ export const T0267_JUAN_4_XIANJIAN_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...T0267_JUAN_4_ANYANG_WOMEN_MERIT_BENEFITS,
   ...T0267_JUAN_4_ANYANG_FINAL_MERIT_BENEFITS,
   ...T0267_JUAN_4_ANYANG_LASTAGE_MERIT_BENEFITS,
+  ...T0267_JUAN_4_ANYANG_CONCLUSION_MERIT_BENEFITS,
 ];
