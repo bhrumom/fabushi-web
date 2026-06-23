81f0cfe9a62821d577cd9fdc30acf5d5f96e900d
bhrum <bhrumom@gmail.com>
2026-06-24 07:21:05 +0800

    Add T0273 juan 1 zongchi merit benefits (#1212)
    * Add T0273 zongchi merit benefits
    
    * Aggregate T0273 zongchi merit benefits

--- frontend/apps/web/src/lib/faliu-merit-benefits-t0273.ts
+++ frontend/apps/web/src/lib/faliu-merit-benefits-t0273.ts
@@ -1,8 +1,10 @@
 import type { FaliuMeritBenefit } from "./faliu-merit-benefits";
 import { T0273_JUAN_1_MERIT_BENEFITS as T0273_JUAN_1_BASE_MERIT_BENEFITS } from "./faliu-merit-benefits-t0273-base";
 import { T0273_JUAN_1_RULAIZANG_MERIT_BENEFITS } from "./faliu-merit-benefits-t0273-rulaizang";
+import { T0273_JUAN_1_ZONGCHI_MERIT_BENEFITS } from "./faliu-merit-benefits-t0273-zongchi";
 
 export const T0273_JUAN_1_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...T0273_JUAN_1_BASE_MERIT_BENEFITS,
   ...T0273_JUAN_1_RULAIZANG_MERIT_BENEFITS,
+  ...T0273_JUAN_1_ZONGCHI_MERIT_BENEFITS,
 ];
