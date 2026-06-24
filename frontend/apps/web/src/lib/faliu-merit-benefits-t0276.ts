457b2a95936f525485bf37a72064b9e46a6ce7c7
bhrum <bhrumom@gmail.com>
2026-06-24 23:20:01 +0800

    Add T0276 Shigongde first four merit benefits (#1239)
    * Add T0276 Shigongde first four merit benefits
    
    * Register T0276 Shigongde first four merit benefits

--- frontend/apps/web/src/lib/faliu-merit-benefits-t0276.ts
+++ frontend/apps/web/src/lib/faliu-merit-benefits-t0276.ts
@@ -2,9 +2,11 @@ import type { FaliuMeritBenefit } from "./faliu-merit-benefits";
 import { T0276_JUAN_1_OPENING_MERIT_BENEFITS } from "./faliu-merit-benefits-t0276-opening";
 import { T0276_JUAN_1_SHUOFAPIN_MERIT_BENEFITS } from "./faliu-merit-benefits-t0276-shuofapin";
 import { T0276_JUAN_1_SHIGONGDE_OPENING_MERIT_BENEFITS } from "./faliu-merit-benefits-t0276-shigongde-opening";
+import { T0276_JUAN_1_SHIGONGDE_FIRST_FOUR_MERIT_BENEFITS } from "./faliu-merit-benefits-t0276-shigongde-first-four";
 
 export const T0276_JUAN_1_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...T0276_JUAN_1_OPENING_MERIT_BENEFITS,
   ...T0276_JUAN_1_SHUOFAPIN_MERIT_BENEFITS,
   ...T0276_JUAN_1_SHIGONGDE_OPENING_MERIT_BENEFITS,
+  ...T0276_JUAN_1_SHIGONGDE_FIRST_FOUR_MERIT_BENEFITS,
 ];
