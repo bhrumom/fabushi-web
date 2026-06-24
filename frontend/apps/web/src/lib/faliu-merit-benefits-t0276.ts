d7b664e805e1175336c44485c6d560581462a277
bhrum <bhrumom@gmail.com>
2026-06-25 02:20:40 +0800

    Add T0276 final circulation merit benefits (#1242)
    * Add T0276 final circulation merit benefits
    
    * Aggregate T0276 final circulation merit benefits

--- frontend/apps/web/src/lib/faliu-merit-benefits-t0276.ts
+++ frontend/apps/web/src/lib/faliu-merit-benefits-t0276.ts
@@ -5,6 +5,7 @@ import { T0276_JUAN_1_SHIGONGDE_OPENING_MERIT_BENEFITS } from "./faliu-merit-ben
 import { T0276_JUAN_1_SHIGONGDE_FIRST_FOUR_MERIT_BENEFITS } from "./faliu-merit-benefits-t0276-shigongde-first-four";
 import { T0276_JUAN_1_SHIGONGDE_FIFTH_TO_SEVENTH_MERIT_BENEFITS } from "./faliu-merit-benefits-t0276-shigongde-fifth-to-seventh";
 import { T0276_JUAN_1_SHIGONGDE_EIGHTH_TO_TENTH_MERIT_BENEFITS } from "./faliu-merit-benefits-t0276-shigongde-eighth-to-tenth";
+import { T0276_JUAN_1_FINAL_CIRCULATION_MERIT_BENEFITS } from "./faliu-merit-benefits-t0276-final-circulation";
 
 export const T0276_JUAN_1_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...T0276_JUAN_1_OPENING_MERIT_BENEFITS,
@@ -13,4 +14,5 @@ export const T0276_JUAN_1_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...T0276_JUAN_1_SHIGONGDE_FIRST_FOUR_MERIT_BENEFITS,
   ...T0276_JUAN_1_SHIGONGDE_FIFTH_TO_SEVENTH_MERIT_BENEFITS,
   ...T0276_JUAN_1_SHIGONGDE_EIGHTH_TO_TENTH_MERIT_BENEFITS,
+  ...T0276_JUAN_1_FINAL_CIRCULATION_MERIT_BENEFITS,
 ];
