a2164d7881917c142d5a03bf0155f4a949b06a5d
bhrum <bhrumom@gmail.com>
2026-06-24 22:20:29 +0800

    Add T0276 Shigongde opening merit benefits (#1238)
    * Add T0276 Shigongde opening merit benefits
    
    * Register T0276 Shigongde opening benefits

--- frontend/apps/web/src/lib/faliu-merit-benefits-t0276.ts
+++ frontend/apps/web/src/lib/faliu-merit-benefits-t0276.ts
@@ -1,8 +1,10 @@
 import type { FaliuMeritBenefit } from "./faliu-merit-benefits";
 import { T0276_JUAN_1_OPENING_MERIT_BENEFITS } from "./faliu-merit-benefits-t0276-opening";
 import { T0276_JUAN_1_SHUOFAPIN_MERIT_BENEFITS } from "./faliu-merit-benefits-t0276-shuofapin";
+import { T0276_JUAN_1_SHIGONGDE_OPENING_MERIT_BENEFITS } from "./faliu-merit-benefits-t0276-shigongde-opening";
 
 export const T0276_JUAN_1_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...T0276_JUAN_1_OPENING_MERIT_BENEFITS,
   ...T0276_JUAN_1_SHUOFAPIN_MERIT_BENEFITS,
+  ...T0276_JUAN_1_SHIGONGDE_OPENING_MERIT_BENEFITS,
 ];
