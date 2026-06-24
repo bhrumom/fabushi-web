9ff4ab8e051bf67d4e1723dcb9b945c03cf5dd78
bhrum <bhrumom@gmail.com>
2026-06-25 01:19:48 +0800

    更新 T0276《無量義經》十功德品第八至第十功德 (#1241)
    * Add T0276 shigongde eighth to tenth benefits
    
    * Register T0276 shigongde eighth to tenth benefits

--- frontend/apps/web/src/lib/faliu-merit-benefits-t0276.ts
+++ frontend/apps/web/src/lib/faliu-merit-benefits-t0276.ts
@@ -4,6 +4,7 @@ import { T0276_JUAN_1_SHUOFAPIN_MERIT_BENEFITS } from "./faliu-merit-benefits-t0
 import { T0276_JUAN_1_SHIGONGDE_OPENING_MERIT_BENEFITS } from "./faliu-merit-benefits-t0276-shigongde-opening";
 import { T0276_JUAN_1_SHIGONGDE_FIRST_FOUR_MERIT_BENEFITS } from "./faliu-merit-benefits-t0276-shigongde-first-four";
 import { T0276_JUAN_1_SHIGONGDE_FIFTH_TO_SEVENTH_MERIT_BENEFITS } from "./faliu-merit-benefits-t0276-shigongde-fifth-to-seventh";
+import { T0276_JUAN_1_SHIGONGDE_EIGHTH_TO_TENTH_MERIT_BENEFITS } from "./faliu-merit-benefits-t0276-shigongde-eighth-to-tenth";
 
 export const T0276_JUAN_1_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...T0276_JUAN_1_OPENING_MERIT_BENEFITS,
@@ -11,4 +12,5 @@ export const T0276_JUAN_1_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...T0276_JUAN_1_SHIGONGDE_OPENING_MERIT_BENEFITS,
   ...T0276_JUAN_1_SHIGONGDE_FIRST_FOUR_MERIT_BENEFITS,
   ...T0276_JUAN_1_SHIGONGDE_FIFTH_TO_SEVENTH_MERIT_BENEFITS,
+  ...T0276_JUAN_1_SHIGONGDE_EIGHTH_TO_TENTH_MERIT_BENEFITS,
 ];
