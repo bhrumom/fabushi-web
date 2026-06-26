963b1529f6c0979fa2d021d673e847ba3e294e9e
bhrum <bhrumom@gmail.com>
2026-06-26 13:21:04 +0800

    更新 T0278 卷三开头功德利益数据 (#1280)
    * Add T0278 juan 3 opening merit benefits
    
    * Register T0278 juan 3 merit benefits

--- frontend/apps/web/src/lib/faliu-merit-benefits.ts
+++ frontend/apps/web/src/lib/faliu-merit-benefits.ts
@@ -93,6 +93,7 @@ export * from "./faliu-merit-benefits-t0278";
 export * from "./faliu-merit-benefits-t0278-shiqi-brahma";
 export * from "./faliu-merit-benefits-t0278-juan2";
 export * from "./faliu-merit-benefits-t0278-juan2-lushena-opening";
+export * from "./faliu-merit-benefits-t0278-juan3";
 
 import { getFaliuMeritBenefits as getBaseFaliuMeritBenefits } from "./faliu-merit-benefits-original";
 import * as t0263Juan10 from "./faliu-merit-benefits-t0263-juan10";
@@ -189,6 +190,7 @@ import * as t0278Juan1 from "./faliu-merit-benefits-t0278";
 import * as t0278Juan1ShiqiBrahma from "./faliu-merit-benefits-t0278-shiqi-brahma";
 import * as t0278Juan2 from "./faliu-merit-benefits-t0278-juan2";
 import * as t0278Juan2LushenaOpening from "./faliu-merit-benefits-t0278-juan2-lushena-opening";
+import * as t0278Juan3 from "./faliu-merit-benefits-t0278-juan3";
 
 const overrideMeritBenefits = {
   "T0263:10": [
@@ -321,6 +323,7 @@ const overrideMeritBenefits = {
     ...t0278Juan2.T0278_JUAN_2_MERIT_BENEFITS,
     ...t0278Juan2LushenaOpening.T0278_JUAN_2_LUSHENA_OPENING_MERIT_BENEFITS,
   ],
+  "T0278:3": t0278Juan3.T0278_JUAN_3_MERIT_BENEFITS,
 };
 
 export function getFaliuMeritBenefits(work: string | null | undefined, juan: string | number | null | undefined) {
