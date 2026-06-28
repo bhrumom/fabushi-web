c861508ee6c97a00e49f2370409c9abffb975069
bhrum <bhrumom@gmail.com>
2026-06-28 23:19:13 +0800

    更新 T0278 卷五十億光明佛境界功德利益 (#1343)
    * Add T0278 juan 5 billion-world light merit benefits
    
    * Wire T0278 juan 5 billion-world light benefits

--- frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan5.ts
+++ frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan5.ts
@@ -1,8 +1,10 @@
 import type { FaliuMeritBenefit } from "./faliu-merit-benefits";
 import { T0278_JUAN_5_MERIT_BENEFITS as T0278_JUAN_5_BASE_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan5-base";
 import { T0278_JUAN_5_LIGHT_HUNDRED_MILLION_WORLDS_FANGBIAN_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan5-light-hundred-million-worlds-fangbian";
+import { T0278_JUAN_5_LIGHT_BILLION_WORLDS_BUDDHA_REALM_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan5-light-billion-worlds-buddha-realm";
 
 export const T0278_JUAN_5_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...T0278_JUAN_5_BASE_MERIT_BENEFITS,
   ...T0278_JUAN_5_LIGHT_HUNDRED_MILLION_WORLDS_FANGBIAN_MERIT_BENEFITS,
+  ...T0278_JUAN_5_LIGHT_BILLION_WORLDS_BUDDHA_REALM_MERIT_BENEFITS,
 ];
