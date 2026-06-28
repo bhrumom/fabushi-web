57fb754e8d61a2a652d16b1c2be58656a18e84c2
bhrum <bhrumom@gmail.com>
2026-06-29 00:19:32 +0800

    更新 T0278 卷五如來光明覺品終段功德利益 (#1345)
    * Add T0278 juan 5 light all worlds merit benefits
    
    * Register T0278 juan 5 light all worlds benefits

--- frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan5.ts
+++ frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan5.ts
@@ -2,9 +2,11 @@ import type { FaliuMeritBenefit } from "./faliu-merit-benefits";
 import { T0278_JUAN_5_MERIT_BENEFITS as T0278_JUAN_5_BASE_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan5-base";
 import { T0278_JUAN_5_LIGHT_HUNDRED_MILLION_WORLDS_FANGBIAN_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan5-light-hundred-million-worlds-fangbian";
 import { T0278_JUAN_5_LIGHT_BILLION_WORLDS_BUDDHA_REALM_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan5-light-billion-worlds-buddha-realm";
+import { T0278_JUAN_5_LIGHT_ALL_WORLDS_CHAPTER_END_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan5-light-all-worlds-chapter-end";
 
 export const T0278_JUAN_5_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...T0278_JUAN_5_BASE_MERIT_BENEFITS,
   ...T0278_JUAN_5_LIGHT_HUNDRED_MILLION_WORLDS_FANGBIAN_MERIT_BENEFITS,
   ...T0278_JUAN_5_LIGHT_BILLION_WORLDS_BUDDHA_REALM_MERIT_BENEFITS,
+  ...T0278_JUAN_5_LIGHT_ALL_WORLDS_CHAPTER_END_MERIT_BENEFITS,
 ];
