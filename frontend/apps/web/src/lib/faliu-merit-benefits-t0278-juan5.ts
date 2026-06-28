233c81ed4cd56669a54d13eb623c35f9286978b4
bhrum <bhrumom@gmail.com>
2026-06-29 01:19:18 +0800

    Add T0278 juan 5 Mingnan Jueshou merit benefits (#1347)
    * Add T0278 juan 5 Mingnan Jueshou merit benefits
    
    * Merge T0278 juan 5 Mingnan Jueshou merit benefits

--- frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan5.ts
+++ frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan5.ts
@@ -3,10 +3,12 @@ import { T0278_JUAN_5_MERIT_BENEFITS as T0278_JUAN_5_BASE_MERIT_BENEFITS } from 
 import { T0278_JUAN_5_LIGHT_HUNDRED_MILLION_WORLDS_FANGBIAN_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan5-light-hundred-million-worlds-fangbian";
 import { T0278_JUAN_5_LIGHT_BILLION_WORLDS_BUDDHA_REALM_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan5-light-billion-worlds-buddha-realm";
 import { T0278_JUAN_5_LIGHT_ALL_WORLDS_CHAPTER_END_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan5-light-all-worlds-chapter-end";
+import { T0278_JUAN_5_BODHISATTVA_MINGNAN_JUESHOU_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan5-bodhisattva-mingnan-jueshou";
 
 export const T0278_JUAN_5_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...T0278_JUAN_5_BASE_MERIT_BENEFITS,
   ...T0278_JUAN_5_LIGHT_HUNDRED_MILLION_WORLDS_FANGBIAN_MERIT_BENEFITS,
   ...T0278_JUAN_5_LIGHT_BILLION_WORLDS_BUDDHA_REALM_MERIT_BENEFITS,
   ...T0278_JUAN_5_LIGHT_ALL_WORLDS_CHAPTER_END_MERIT_BENEFITS,
+  ...T0278_JUAN_5_BODHISATTVA_MINGNAN_JUESHOU_MERIT_BENEFITS,
 ];
