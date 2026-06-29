4f068fb472b28975d48d281420fb69bb275b794d
bhrum <bhrumom@gmail.com>
2026-06-29 11:18:33 +0800

    Add T0278 juan 5 Buddha power all-seen merit benefit (#1360)
    * Add T0278 juan 5 Buddha power all-seen merit benefits
    
    * Fix T0278 juan 5 Buddha power quote punctuation
    
    * Wire T0278 juan 5 Buddha power all-seen merit benefits

--- frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan5.ts
+++ frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan5.ts
@@ -13,6 +13,7 @@ import { T0278_JUAN_5_BODHISATTVA_MINGNAN_FASHOU_MERIT_BENEFITS } from "./faliu-
 import { T0278_JUAN_5_BODHISATTVA_MINGNAN_ZHISHOU_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan5-bodhisattva-mingnan-zhishou";
 import { T0278_JUAN_5_BODHISATTVA_MINGNAN_XIANSHOU_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan5-bodhisattva-mingnan-xianshou";
 import { T0278_JUAN_5_BODHISATTVA_MINGNAN_FOJINGJIE_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan5-bodhisattva-mingnan-fojingjie";
+import { T0278_JUAN_5_BUDDHA_POWER_ALL_SEEN_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan5-buddha-power-all-seen";
 
 export const T0278_JUAN_5_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...T0278_JUAN_5_BASE_MERIT_BENEFITS,
@@ -29,4 +30,5 @@ export const T0278_JUAN_5_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...T0278_JUAN_5_BODHISATTVA_MINGNAN_ZHISHOU_MERIT_BENEFITS,
   ...T0278_JUAN_5_BODHISATTVA_MINGNAN_XIANSHOU_MERIT_BENEFITS,
   ...T0278_JUAN_5_BODHISATTVA_MINGNAN_FOJINGJIE_MERIT_BENEFITS,
+  ...T0278_JUAN_5_BUDDHA_POWER_ALL_SEEN_MERIT_BENEFITS,
 ];
