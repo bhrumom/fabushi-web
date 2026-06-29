2e710f11eadf7804d9b7cebc242c3360a440437a
bhrum <bhrumom@gmail.com>
2026-06-29 08:18:42 +0800

    Add T0278 juan 5 Zhishou merit benefits (#1355)
    * Add T0278 juan 5 Zhishou merit benefits
    
    * Wire T0278 juan 5 Zhishou merit benefits

--- frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan5.ts
+++ frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan5.ts
@@ -10,6 +10,7 @@ import { T0278_JUAN_5_BODHISATTVA_MINGNAN_DESHOU_MERIT_BENEFITS } from "./faliu-
 import { T0278_JUAN_5_BODHISATTVA_MINGNAN_MUSHOU_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan5-bodhisattva-mingnan-mushou";
 import { T0278_JUAN_5_BODHISATTVA_MINGNAN_JINSHOU_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan5-bodhisattva-mingnan-jinshou";
 import { T0278_JUAN_5_BODHISATTVA_MINGNAN_FASHOU_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan5-bodhisattva-mingnan-fashou";
+import { T0278_JUAN_5_BODHISATTVA_MINGNAN_ZHISHOU_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan5-bodhisattva-mingnan-zhishou";
 
 export const T0278_JUAN_5_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...T0278_JUAN_5_BASE_MERIT_BENEFITS,
@@ -23,4 +24,5 @@ export const T0278_JUAN_5_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...T0278_JUAN_5_BODHISATTVA_MINGNAN_MUSHOU_MERIT_BENEFITS,
   ...T0278_JUAN_5_BODHISATTVA_MINGNAN_JINSHOU_MERIT_BENEFITS,
   ...T0278_JUAN_5_BODHISATTVA_MINGNAN_FASHOU_MERIT_BENEFITS,
+  ...T0278_JUAN_5_BODHISATTVA_MINGNAN_ZHISHOU_MERIT_BENEFITS,
 ];
