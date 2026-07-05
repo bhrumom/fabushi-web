5d08df56231d7c5fd1854d1bc0dddba193452fee
bhrum <bhrumom@gmail.com>
2026-07-06 05:19:03 +0800

    Add T0278 juan 12 ten treasuries conclusion benefits (#1517)
    * Add T0278 juan 12 ten treasuries conclusion benefits
    
    * Wire T0278 juan 12 ten treasuries conclusion benefits

--- frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan12.ts
+++ frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan12.ts
@@ -1,7 +1,9 @@
 import { T0278_JUAN_12_MERIT_BENEFITS as T0278_JUAN_12_BASE_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan12-base";
 import { T0278_JUAN_12_INEXHAUSTIBLE_ELOQUENCE_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan12-inexhaustible-eloquence";
+import { T0278_JUAN_12_TEN_TREASURIES_CONCLUSION_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan12-ten-treasuries-conclusion";
 
 export const T0278_JUAN_12_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...T0278_JUAN_12_BASE_MERIT_BENEFITS,
   ...T0278_JUAN_12_INEXHAUSTIBLE_ELOQUENCE_MERIT_BENEFITS,
+  ...T0278_JUAN_12_TEN_TREASURIES_CONCLUSION_MERIT_BENEFITS,
 ];
