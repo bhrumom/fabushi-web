25df853f08b641b8698545f350a8a4cc8cb19ec1
bhrum <bhrumom@gmail.com>
2026-07-11 14:19:43 +0800

    Add T0278 juan 15 to-all-places verse merit benefits (#1565)
    * Add T0278 juan 15 to-all-places verse benefits
    
    * Wire T0278 juan 15 to-all-places verse benefits

--- frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan15.ts
+++ frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan15.ts
@@ -3,12 +3,14 @@ export * from "./faliu-merit-benefits-t0278-juan15-equal-buddhas-verses";
 export * from "./faliu-merit-benefits-t0278-juan15-to-all-places-opening";
 export * from "./faliu-merit-benefits-t0278-juan15-to-all-places-offerings";
 export * from "./faliu-merit-benefits-t0278-juan15-to-all-places-dharma-inclusion";
+export * from "./faliu-merit-benefits-t0278-juan15-to-all-places-verses";
 
 import * as t0278Juan15Base from "./faliu-merit-benefits-t0278-juan15-base";
 import * as t0278Juan15EqualBuddhasVerses from "./faliu-merit-benefits-t0278-juan15-equal-buddhas-verses";
 import * as t0278Juan15ToAllPlacesOpening from "./faliu-merit-benefits-t0278-juan15-to-all-places-opening";
 import * as t0278Juan15ToAllPlacesOfferings from "./faliu-merit-benefits-t0278-juan15-to-all-places-offerings";
 import * as t0278Juan15ToAllPlacesDharmaInclusion from "./faliu-merit-benefits-t0278-juan15-to-all-places-dharma-inclusion";
+import * as t0278Juan15ToAllPlacesVerses from "./faliu-merit-benefits-t0278-juan15-to-all-places-verses";
 
 export const T0278_JUAN_15_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...t0278Juan15Base.T0278_JUAN_15_MERIT_BENEFITS,
@@ -16,4 +18,5 @@ export const T0278_JUAN_15_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...t0278Juan15ToAllPlacesOpening.T0278_JUAN_15_TO_ALL_PLACES_OPENING_MERIT_BENEFITS,
   ...t0278Juan15ToAllPlacesOfferings.T0278_JUAN_15_TO_ALL_PLACES_OFFERINGS_MERIT_BENEFITS,
   ...t0278Juan15ToAllPlacesDharmaInclusion.T0278_JUAN_15_TO_ALL_PLACES_DHARMA_INCLUSION_MERIT_BENEFITS,
+  ...t0278Juan15ToAllPlacesVerses.T0278_JUAN_15_TO_ALL_PLACES_VERSES_MERIT_BENEFITS,
 ];
