484db0c0a6bbc13dcc619a101c315848b356fe38
bhrum <bhrumom@gmail.com>
2026-07-11 08:17:34 +0800

    Add T0278 juan 15 to-all-places opening merit benefits (#1562)
    * Add T0278 juan 15 to-all-places opening merit benefits
    
    * Wire T0278 juan 15 to-all-places opening merit benefits

--- frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan15.ts
+++ frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan15.ts
@@ -1,10 +1,13 @@
 export * from "./faliu-merit-benefits-t0278-juan15-base";
 export * from "./faliu-merit-benefits-t0278-juan15-equal-buddhas-verses";
+export * from "./faliu-merit-benefits-t0278-juan15-to-all-places-opening";
 
 import * as t0278Juan15Base from "./faliu-merit-benefits-t0278-juan15-base";
 import * as t0278Juan15EqualBuddhasVerses from "./faliu-merit-benefits-t0278-juan15-equal-buddhas-verses";
+import * as t0278Juan15ToAllPlacesOpening from "./faliu-merit-benefits-t0278-juan15-to-all-places-opening";
 
 export const T0278_JUAN_15_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...t0278Juan15Base.T0278_JUAN_15_MERIT_BENEFITS,
   ...t0278Juan15EqualBuddhasVerses.T0278_JUAN_15_EQUAL_BUDDHAS_VERSES_MERIT_BENEFITS,
+  ...t0278Juan15ToAllPlacesOpening.T0278_JUAN_15_TO_ALL_PLACES_OPENING_MERIT_BENEFITS,
 ];
