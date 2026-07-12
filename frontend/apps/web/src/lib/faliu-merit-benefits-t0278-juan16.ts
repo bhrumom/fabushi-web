5427e41734a5c27fad8dce5fa4512d88f3f32170
bhrum <bhrumom@gmail.com>
2026-07-12 13:19:19 +0800

    Add T0278 juan 16 jeweled carts merit benefits (#1580)
    * Add T0278 juan 16 jeweled carts merit benefits
    
    * Wire T0278 juan 16 jeweled carts merit benefits

--- frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan16.ts
+++ frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan16.ts
@@ -4,6 +4,7 @@ export * from "./faliu-merit-benefits-t0278-juan16-dwellings";
 export * from "./faliu-merit-benefits-t0278-juan16-lamp-offering";
 export * from "./faliu-merit-benefits-t0278-juan16-medicine-offering";
 export * from "./faliu-merit-benefits-t0278-juan16-vessels";
+export * from "./faliu-merit-benefits-t0278-juan16-jeweled-carts-buddhas";
 
 import * as t0278Juan16Base from "./faliu-merit-benefits-t0278-juan16-base";
 import * as t0278Juan16DrinkThroughSeats from "./faliu-merit-benefits-t0278-juan16-drink-through-seats";
@@ -11,6 +12,7 @@ import * as t0278Juan16Dwellings from "./faliu-merit-benefits-t0278-juan16-dwell
 import * as t0278Juan16LampOffering from "./faliu-merit-benefits-t0278-juan16-lamp-offering";
 import * as t0278Juan16MedicineOffering from "./faliu-merit-benefits-t0278-juan16-medicine-offering";
 import * as t0278Juan16Vessels from "./faliu-merit-benefits-t0278-juan16-vessels";
+import * as t0278Juan16JeweledCartsBuddhas from "./faliu-merit-benefits-t0278-juan16-jeweled-carts-buddhas";
 
 export const T0278_JUAN_16_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...t0278Juan16Base.T0278_JUAN_16_BASE_MERIT_BENEFITS,
@@ -19,4 +21,5 @@ export const T0278_JUAN_16_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...t0278Juan16LampOffering.T0278_JUAN_16_LAMP_OFFERING_MERIT_BENEFITS,
   ...t0278Juan16MedicineOffering.T0278_JUAN_16_MEDICINE_OFFERING_MERIT_BENEFITS,
   ...t0278Juan16Vessels.T0278_JUAN_16_VESSELS_MERIT_BENEFITS,
+  ...t0278Juan16JeweledCartsBuddhas.T0278_JUAN_16_JEWELED_CARTS_BUDDHAS_MERIT_BENEFITS,
 ];
