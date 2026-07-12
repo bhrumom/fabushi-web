ebd0404074ee2817af3d171f5b614192869ea52b
bhrum <bhrumom@gmail.com>
2026-07-12 08:19:59 +0800

    Add T0278 juan 16 lamp offering merit benefits (#1574)
    * Add T0278 juan 16 lamp offering merit benefits
    
    * Include T0278 juan 16 lamp offering benefits

--- frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan16.ts
+++ frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan16.ts
@@ -1,13 +1,16 @@
 export * from "./faliu-merit-benefits-t0278-juan16-base";
 export * from "./faliu-merit-benefits-t0278-juan16-drink-through-seats";
 export * from "./faliu-merit-benefits-t0278-juan16-dwellings";
+export * from "./faliu-merit-benefits-t0278-juan16-lamp-offering";
 
 import * as t0278Juan16Base from "./faliu-merit-benefits-t0278-juan16-base";
 import * as t0278Juan16DrinkThroughSeats from "./faliu-merit-benefits-t0278-juan16-drink-through-seats";
 import * as t0278Juan16Dwellings from "./faliu-merit-benefits-t0278-juan16-dwellings";
+import * as t0278Juan16LampOffering from "./faliu-merit-benefits-t0278-juan16-lamp-offering";
 
 export const T0278_JUAN_16_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...t0278Juan16Base.T0278_JUAN_16_BASE_MERIT_BENEFITS,
   ...t0278Juan16DrinkThroughSeats.T0278_JUAN_16_DRINK_THROUGH_SEATS_MERIT_BENEFITS,
   ...t0278Juan16Dwellings.T0278_JUAN_16_DWELLINGS_MERIT_BENEFITS,
+  ...t0278Juan16LampOffering.T0278_JUAN_16_LAMP_OFFERING_MERIT_BENEFITS,
 ];
