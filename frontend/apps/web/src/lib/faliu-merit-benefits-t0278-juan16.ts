26d193c9cc77bfec1a04eb8b4de9ba8f6cf30b73
bhrum <bhrumom@gmail.com>
2026-07-13 05:18:48 +0800

    Add T0278 juan 16 various seats merit benefits (#1586)
    * Add T0278 juan 16 various seats merit benefits
    
    * Register T0278 juan 16 various seats merit benefits

--- frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan16.ts
+++ frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan16.ts
@@ -10,6 +10,7 @@ export * from "./faliu-merit-benefits-t0278-juan16-rulai-vehicles";
 export * from "./faliu-merit-benefits-t0278-juan16-sravaka-pratyekabuddha-vehicles";
 export * from "./faliu-merit-benefits-t0278-juan16-all-fields-vehicles";
 export * from "./faliu-merit-benefits-t0278-juan16-elephant-horse-vehicles";
+export * from "./faliu-merit-benefits-t0278-juan16-various-seats";
 
 import * as t0278Juan16Base from "./faliu-merit-benefits-t0278-juan16-base";
 import * as t0278Juan16DrinkThroughSeats from "./faliu-merit-benefits-t0278-juan16-drink-through-seats";
@@ -23,6 +24,7 @@ import * as t0278Juan16RulaiVehicles from "./faliu-merit-benefits-t0278-juan16-r
 import * as t0278Juan16SravakaPratyekabuddhaVehicles from "./faliu-merit-benefits-t0278-juan16-sravaka-pratyekabuddha-vehicles";
 import * as t0278Juan16AllFieldsVehicles from "./faliu-merit-benefits-t0278-juan16-all-fields-vehicles";
 import * as t0278Juan16ElephantHorseVehicles from "./faliu-merit-benefits-t0278-juan16-elephant-horse-vehicles";
+import * as t0278Juan16VariousSeats from "./faliu-merit-benefits-t0278-juan16-various-seats";
 
 export const T0278_JUAN_16_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...t0278Juan16Base.T0278_JUAN_16_BASE_MERIT_BENEFITS,
@@ -37,4 +39,5 @@ export const T0278_JUAN_16_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...t0278Juan16SravakaPratyekabuddhaVehicles.T0278_JUAN_16_SRAVAKA_PRATYEKABUDDHA_VEHICLES_MERIT_BENEFITS,
   ...t0278Juan16AllFieldsVehicles.T0278_JUAN_16_ALL_FIELDS_VEHICLES_MERIT_BENEFITS,
   ...t0278Juan16ElephantHorseVehicles.T0278_JUAN_16_ELEPHANT_HORSE_VEHICLES_MERIT_BENEFITS,
+  ...t0278Juan16VariousSeats.T0278_JUAN_16_VARIOUS_SEATS_MERIT_BENEFITS,
 ];
