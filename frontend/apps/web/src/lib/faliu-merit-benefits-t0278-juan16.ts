60f824b24e44472672b3ec7afbcedda1bb7a927c
bhrum <bhrumom@gmail.com>
2026-07-12 10:19:02 +0800

    Add T0278 juan 16 vessels merit benefits (#1576)
    * Add T0278 juan 16 vessels merit benefits
    
    * Include T0278 juan 16 vessels merit benefits

--- frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan16.ts
+++ frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan16.ts
@@ -3,12 +3,14 @@ export * from "./faliu-merit-benefits-t0278-juan16-drink-through-seats";
 export * from "./faliu-merit-benefits-t0278-juan16-dwellings";
 export * from "./faliu-merit-benefits-t0278-juan16-lamp-offering";
 export * from "./faliu-merit-benefits-t0278-juan16-medicine-offering";
+export * from "./faliu-merit-benefits-t0278-juan16-vessels";
 
 import * as t0278Juan16Base from "./faliu-merit-benefits-t0278-juan16-base";
 import * as t0278Juan16DrinkThroughSeats from "./faliu-merit-benefits-t0278-juan16-drink-through-seats";
 import * as t0278Juan16Dwellings from "./faliu-merit-benefits-t0278-juan16-dwellings";
 import * as t0278Juan16LampOffering from "./faliu-merit-benefits-t0278-juan16-lamp-offering";
 import * as t0278Juan16MedicineOffering from "./faliu-merit-benefits-t0278-juan16-medicine-offering";
+import * as t0278Juan16Vessels from "./faliu-merit-benefits-t0278-juan16-vessels";
 
 export const T0278_JUAN_16_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...t0278Juan16Base.T0278_JUAN_16_BASE_MERIT_BENEFITS,
@@ -16,4 +18,5 @@ export const T0278_JUAN_16_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...t0278Juan16Dwellings.T0278_JUAN_16_DWELLINGS_MERIT_BENEFITS,
   ...t0278Juan16LampOffering.T0278_JUAN_16_LAMP_OFFERING_MERIT_BENEFITS,
   ...t0278Juan16MedicineOffering.T0278_JUAN_16_MEDICINE_OFFERING_MERIT_BENEFITS,
+  ...t0278Juan16Vessels.T0278_JUAN_16_VESSELS_MERIT_BENEFITS,
 ];
