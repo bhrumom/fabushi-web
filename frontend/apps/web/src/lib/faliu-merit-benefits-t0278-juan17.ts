d3f2662ce59eb9318c530419481a8a83a5bded1e
bhrum <bhrumom@gmail.com>
2026-07-13 22:16:46 +0800

    Add T0278 juan 17 tongue merit benefits (#1603)
    * Add T0278 juan 17 tongue merit benefits
    
    * Wire T0278 juan 17 tongue merit benefits

--- frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan17.ts
+++ frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan17.ts
@@ -5,6 +5,7 @@ import { T0278_JUAN_17_DEATH_ROW_RESCUE_MERIT_BENEFITS } from "./faliu-merit-ben
 import { T0278_JUAN_17_EARS_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan17-ears";
 import { T0278_JUAN_17_NOSE_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan17-nose";
 import { T0278_JUAN_17_TEETH_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan17-teeth";
+import { T0278_JUAN_17_TONGUE_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan17-tongue";
 import { T0278_JUAN_17_EYES_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan17-eyes";
 import { T0278_JUAN_17_HAIR_JEWEL_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan17-hair-jewel";
 import { T0278_JUAN_17_PRISON_RESCUE_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan17-prison-rescue";
@@ -62,4 +63,5 @@ export const T0278_JUAN_17_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...T0278_JUAN_17_EARS_MERIT_BENEFITS,
   ...T0278_JUAN_17_NOSE_MERIT_BENEFITS,
   ...T0278_JUAN_17_TEETH_MERIT_BENEFITS,
+  ...T0278_JUAN_17_TONGUE_MERIT_BENEFITS,
 ];
