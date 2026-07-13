96d37660ae3b98f9ee68dbf6494ffa1d10436321
bhrum <bhrumom@gmail.com>
2026-07-13 12:20:54 +0800

    Add T0278 juan 17 death row rescue merit benefits (#1593)
    * Add T0278 juan 17 death row rescue merit benefits
    
    * Wire T0278 juan 17 death row rescue benefits

--- frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan17.ts
+++ frontend/apps/web/src/lib/faliu-merit-benefits-t0278-juan17.ts
@@ -1,6 +1,7 @@
 import { T0278_JUAN_17_ADORNMENTS_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan17-adornments";
 import { T0278_JUAN_17_BANNERS_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan17-banners";
 import { T0278_JUAN_17_CROWN_JEWEL_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan17-crown-jewel";
+import { T0278_JUAN_17_DEATH_ROW_RESCUE_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan17-death-row-rescue";
 import { T0278_JUAN_17_PRISON_RESCUE_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan17-prison-rescue";
 import { T0278_JUAN_17_TREASURY_MERIT_BENEFITS } from "./faliu-merit-benefits-t0278-juan17-treasury";
 
@@ -50,4 +51,5 @@ export const T0278_JUAN_17_MERIT_BENEFITS: FaliuMeritBenefit[] = [
   ...T0278_JUAN_17_ADORNMENTS_MERIT_BENEFITS,
   ...T0278_JUAN_17_CROWN_JEWEL_MERIT_BENEFITS,
   ...T0278_JUAN_17_PRISON_RESCUE_MERIT_BENEFITS,
+  ...T0278_JUAN_17_DEATH_ROW_RESCUE_MERIT_BENEFITS,
 ];
